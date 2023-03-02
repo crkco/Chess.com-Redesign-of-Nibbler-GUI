fef36e5e0173579e5a74dcf739eeb3a787fcbba6
crkco <cu29p@hotmail.com>
2023-03-03 00:00:42 +0100

    Add files via upload

---
+++ renderer/55_winrate_graph.js
@@ -0,0 +1,343 @@
+"use strict";
+
+
+var yValues_ = [ 0.519, 0.5, 0.5, 0.47, 0.471, 0.46, 0.5, 0.5, 0.7, 0.5, 0.6, 0.55, 0.51, 0.505, 0.5, 0.5, 0.4, 0.99, 0.2, 0.5, 0.49, 0.51, 0.52, 0.5, 0.5, 0.5, 0.5, 0.51, 0.505, 0.5, 0.5, 0.51, 0.99, 0.99, 0.98, 0.95, 0.99, 0.6, 0.99, 0.7, 0.75, 0.51, 0.515, 0.5, 0.51, 0.5, 0.99, 0.5, 0.8, 0.75, 0.8, 0.75, 0.8, 0.75, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99 ];
+var yValues = [];
+
+/*for(let i = 0; i < 2; i++) {
+  yValues.push(null);
+}*/
+
+for(let i = 0; i < yValues_.length; i++) {
+	yValues.push(yValues_[i]);
+}
+
+const Highcharts = require("Highcharts");
+
+var hchart = Highcharts.chart('graph', {
+    chart: {
+        type: 'areaspline',
+        backgroundColor: '#484845',
+        spacing: [0, 0, 0, 0],
+        margin: 0,
+        reflow: true,
+		events: {
+			click: function(event) { hub.winrate_click(event); }
+		}
+    },
+    title: {
+      text: ''
+    },
+    xAxis: {
+      labels: {
+        enabled: false
+      },
+      title: false,
+      gridLineWidth: 0,
+      minorGridLineWidth: 1,
+      minorGridLineColor: '#4c4c49',
+      minorTickInterval: 4,
+      minorTicks: true,
+      tickLength: 0,
+      lineColor: '#ffffff00',
+      endOnTick:false,
+      startOnTick:false,
+    },
+    yAxis: {
+      labels: {
+        enabled: false
+      },
+      gridLineWidth: 0,
+      minorGridLineWidth: 1,
+      minorGridLineColor: '#4c4c49',
+      minorTickInterval: 0.1,
+      minorTicks: true,
+      maxPadding: 0,
+      title: false,
+      max: 1.1,
+      min: -0.1,
+      tickLength: 0,
+      endOnTick:false,
+      startOnTick:false,
+    },
+    plotOptions: {
+      series: {
+        dataLabels: {
+          enabled: false
+        }
+      },
+      line: {
+      	lineColor: 'white', // Change this value to the desired color
+    	},
+    },
+    credits: {
+      enabled: false
+    },
+    legend: {
+      enabled: false
+    },
+    series: [{
+        data: yValues,
+        type: 'areaspline',
+        color: '#cccccc',
+        fillColor: '#999999',
+        threshold: 0.5,
+        negativeFillColor: '#666666',
+        negativeColor: '#808080',
+        marker: {
+        	enabled: true,
+			radius: 0.1,
+        },
+		point: {
+			events: {
+				//click: function(event) { graphClick(event); }
+		   }
+	   }
+    }]
+});
+
+function NewGrapher() {
+
+	let grapher = Object.create(null);
+	
+	grapher.dragging = false;			// Used by the event handlers in start.js
+
+	grapher.clear_graph = function() {
+
+		let boundingrect = graph.getBoundingClientRect();
+		let width = window.innerWidth - boundingrect.left - 16;
+		let height = boundingrect.bottom - boundingrect.top;
+
+		// This clears the canvas...
+
+		graph.width = width;
+		graph.height = height;
+	};
+
+	grapher.draw = function(node, force) {
+		if (config.graph_height <= 0) {
+			return;
+		}
+		this.draw_everything(node);
+	};
+
+var lastMarkerIndex = -1;
+
+	grapher.draw_everything = function(node) {
+
+
+		//this.clear_graph();
+		let width = graph.width;		// After the above.
+		let height = graph.height;
+
+		let eval_list = node.future_eval_history();
+		var yValues = [];
+
+		for(let i = 0; i < eval_list.length; i++) {
+			if(eval_list[i] !== null) {
+				yValues.push(eval_list[i]);
+			} else {
+				yValues.push(null);
+			}
+
+			if(yValues[i] !== null) {
+				if(hub.tree.node !== null && hub.tree.node !== undefined && hub.tree.node.depth === i) {
+					if(lastMarkerIndex !== i) {
+						yValues[i] = {y:yValues[i], marker: {radius:6,fillColor:"orange",lineColor:"orange",hover:null}};
+					} else {
+						lastMarkerIndex = i;
+					}
+				} else {
+					yValues[i] = {y:yValues[i], marker: {radius:1,hover:null}};
+				}
+			}
+		}
+
+		if(yValues.length < 40) {
+			for(let j = 0; j < (40 - yValues.length); j++) {
+				yValues.push(null);
+			}
+		}
+
+		hchart.series[0].setData(yValues);
+		hchart.redraw(false);
+
+		//let runs = this.make_runs(eval_list, width, height, node.graph_length_knower.val);
+		return;
+		// Draw our normal runs...
+
+		graphctx.strokeStyle = "white";
+		graphctx.lineWidth = config.graph_line_width;
+		graphctx.setLineDash([]);
+
+		for (let run of runs.normal_runs) {
+			graphctx.beginPath();
+			graphctx.moveTo(run[0].x1, run[0].y1);
+			for (let edge of run) {
+				graphctx.lineTo(edge.x2, edge.y2);
+			}
+			graphctx.stroke();
+		}
+
+		// Draw our dashed runs...
+
+		graphctx.strokeStyle = "#999999";
+		graphctx.lineWidth = config.graph_line_width;
+		graphctx.setLineDash([config.graph_line_width, config.graph_line_width]);
+
+		for (let run of runs.dashed_runs) {
+			graphctx.beginPath();
+			graphctx.moveTo(run[0].x1, run[0].y1);
+			for (let edge of run) {
+				graphctx.lineTo(edge.x2, edge.y2);
+			}
+			graphctx.stroke();
+		}
+	};
+
+	grapher.make_runs = function(eval_list, width, height, graph_length) {
+
+		// Returns an object with 2 arrays (normal_runs and dashed_runs).
+		// Each of those is an array of arrays of contiguous edges that can be drawn at once.
+
+		let all_edges = [];
+
+		let last_x = null;
+		let last_y = null;
+		let last_n = null;
+
+		// This loop creates all edges that we are going to draw, and marks each
+		// edge as dashed or not...
+
+		for (let n = 0; n < eval_list.length; n++) {
+
+			let e = eval_list[n];
+
+			if (e !== null) {
+
+				let x = width * n / graph_length;
+
+				let y = (1 - e) * height;
+				if (y < 1) y = 1;
+				if (y > height - 2) y = height - 2;
+
+				if (last_x !== null) {
+					all_edges.push({
+						x1: last_x,
+						y1: last_y,
+						x2: x,
+						y2: y,
+						dashed: n - last_n !== 1,
+					});
+				}
+
+				last_x = x;
+				last_y = y;
+				last_n = n;
+			}
+		}
+
+		// Now we make runs of contiguous edges that share a style...
+
+		let normal_runs = [];
+		let dashed_runs = [];
+
+		let run = [];
+		let current_meta_list = normal_runs;	// Will point at normal_runs or dashed_runs.
+
+		for (let edge of all_edges) {
+			if ((edge.dashed && current_meta_list !== dashed_runs) || (!edge.dashed && current_meta_list !== normal_runs)) {
+				if (run.length > 0) {
+					current_meta_list.push(run);
+				}
+				current_meta_list = edge.dashed ? dashed_runs : normal_runs;
+				run = [];
+			}
+			run.push(edge);
+		}
+		if (run.length > 0) {
+			current_meta_list.push(run);
+		}
+
+		return {normal_runs, dashed_runs};
+	};
+
+	grapher.draw_50_percent_line = function(width, height) {
+		return;
+
+		// Avoid anti-aliasing... (FIXME: we assumed graph size was even)
+		let pixel_y_adjustment = config.graph_line_width % 2 === 0 ? 0 : -0.5;
+
+		graphctx.strokeStyle = "#666666";
+		graphctx.lineWidth = config.graph_line_width;
+		graphctx.setLineDash([config.graph_line_width, config.graph_line_width]);
+		graphctx.beginPath();
+		graphctx.moveTo(0, height / 2 + pixel_y_adjustment);
+		graphctx.lineTo(width, height / 2 + pixel_y_adjustment);
+		graphctx.stroke();
+	};
+
+	grapher.draw_position_line = function(eval_list_length, node) {
+		return;
+
+		if (eval_list_length < 2) {
+			return;
+		}
+
+		let width = graph.width;
+		let height = graph.height;
+
+		// Avoid anti-aliasing with x value, line up with 50 percent line (above) with y value...
+		let pixel_x_adjustment = config.graph_line_width % 2 === 0 ? 0 : 0.5;
+		let pixel_y_adjustment = config.graph_line_width % 2 === 0 ? 0 : -0.5;
+
+		let x = Math.floor(width * node.depth / node.graph_length_knower.val) + pixel_x_adjustment;
+
+		graphctx.strokeStyle = node.is_main_line() ? "#6cccee" : "#ffff00";
+		graphctx.lineWidth = config.graph_line_width;
+		graphctx.setLineDash([config.graph_line_width, config.graph_line_width]);
+
+		graphctx.beginPath();
+		graphctx.moveTo(x, height / 2 + pixel_y_adjustment - config.graph_line_width - 1);
+		graphctx.lineTo(x, 0);
+		graphctx.stroke();
+
+		graphctx.beginPath();
+		graphctx.moveTo(x, height / 2 + pixel_y_adjustment + config.graph_line_width + 1);
+		graphctx.lineTo(x, height);
+		graphctx.stroke();
+	};
+
+	grapher.node_from_click = function(node, event) {
+
+		/*if (!event || config.graph_height <= 0) {
+			return null;
+		}
+
+		let mousex = event.offsetX;
+		if (typeof mousex !== "number") {
+			return null;
+		}
+
+		let width = graph.width;
+		if (typeof width !== "number" || width < 1) {
+			return null;
+		}*/
+
+		let node_list = node.future_node_history();
+		if (node_list.length === 0) {
+			return null;
+		}
+
+		// OK, everything is valid...
+
+		let click_depth = Math.round(event.xAxis[0].value);
+
+		if (click_depth < 0) click_depth = 0;
+		if (click_depth >= node_list.length) click_depth = node_list.length - 1;
+
+		return node_list[click_depth];
+	};
+
+	return grapher;
+}
---
+++ renderer/72_tree_draw.js
@@ -0,0 +1,191 @@
+"use strict";
+
+let tree_draw_props = {
+
+	// Since we use Object.assign(), it's bad form to have any deep objects in the props.
+
+	ordered_nodes_cache: null,
+	ordered_nodes_cache_version: -1,
+
+	dom_easy_highlight_change: function() {
+
+		// When the previously highlighted node and the newly highlighted node are on the same line,
+		// with the same end-of-line, meaning no gray / white changes are needed.
+
+		let dom_highlight = this.get_movelist_highlight();
+		let highlight_class;
+
+		if (dom_highlight && dom_highlight.classList.contains("movelist_highlight_yellow")) {
+			highlight_class = "movelist_highlight_yellow";
+		} else {
+			highlight_class = "movelist_highlight_blue";
+		}
+
+		if (dom_highlight) {
+			dom_highlight.classList.remove("movelist_highlight_blue");
+			dom_highlight.classList.remove("movelist_highlight_yellow");
+		}
+
+		let dom_node = document.getElementById(`node_${this.node.id}`);
+
+		if (dom_node) {
+			dom_node.classList.add(highlight_class);
+		}
+
+		this.fix_scrollbar_position();
+	},
+
+	dom_from_scratch: function() {
+
+		// Some prep-work (we need to undo all this at the end)...
+
+		let line_end = this.node.get_end();
+
+		let foo = line_end;
+		while (foo) {
+			foo.current_line = true;	// These nodes will be coloured white, others gray
+			foo = foo.parent;
+		}
+
+		let main_line_end = this.root.get_end();
+		main_line_end.main_line_end = true;
+
+		// Begin...
+
+		if (this.ordered_nodes_cache_version !== this.tree_version) {
+			this.ordered_nodes_cache = get_ordered_nodes(this.root);
+			this.ordered_nodes_cache_version = this.tree_version;
+		}
+
+		let pseudoelements = [];		// Objects containing opening span string `<span foo>` and text string
+
+		for (let item of this.ordered_nodes_cache) {
+
+			if (item === this.root) {
+				continue;
+			}
+
+			// As a crude hack, the item can be a bracket string.
+			// Deal with that first...
+
+			if (typeof item === "string") {
+				pseudoelements.push({
+					opener: "",
+					text: item,
+					closer: ""
+				});
+				continue;
+			}
+
+			// So item is a real node...
+
+			let node = item;
+			let classes = [];
+
+			let number_text = node.token_only_number();
+			
+			if(node.token_only_number() !== " ") {
+				pseudoelements.push({
+					opener: `<span class="movelist_number">`,
+					text: number_text,
+					closer: `</span>`
+				});
+			}
+
+			let move_text = node.token_only_move();
+
+			let additional_class = "";
+
+			if(move_text.includes("N")) { additional_class = "knight" } else
+			if(move_text.includes("Q")) { additional_class = "queen" } else
+			if(move_text.includes("R")) { additional_class = "rook" } else
+			if(move_text.includes("B")) { additional_class = "bishop" } else
+			if(move_text.includes("K")) { additional_class = "king" }
+
+			if(additional_class !== "") {
+				if(number_text !== "") {
+					additional_class += "-white";
+				} else {
+					additional_class += "-black";
+				}
+
+				classes.push("character_pieces");
+				classes.push(additional_class);
+			}
+
+			if (node === this.node) {
+				if (node.is_main_line()) {
+					classes.push("movelist_highlight_blue");
+				} else {
+					classes.push("movelist_highlight_yellow");
+				}
+			}
+
+			pseudoelements.push({
+				opener: `<span class="${classes.join(" ")}" id="node_${node.id}">`,
+				text: node.token_only_move().replace(/N|Q|R|B|K/g, ''),
+				closer: `</span>`
+			});
+		}
+
+		let all_spans = [];
+
+		for (let n = 0; n < pseudoelements.length; n++) {
+
+			let p = pseudoelements[n];
+			let nextp = pseudoelements[n + 1];		// Possibly undefined
+
+			if ((!nextp || (p.text !== "(" && nextp.text !== ")")) && p.text !== "") {
+				p.text += " ";
+			}
+
+			all_spans.push(`${p.opener}${p.text}${p.closer}`);
+		}
+
+		movelist.innerHTML = all_spans.join("");
+
+		// Undo the damage to our tree from the start...
+
+		foo = line_end;
+		while(foo) {
+			delete foo.current_line;
+			foo = foo.parent;
+		}
+
+		delete main_line_end.main_line_end;
+
+		// And finally...
+
+		this.fix_scrollbar_position();
+	},
+
+	// Helpers...
+
+	get_movelist_highlight: function() {
+		let elements = document.getElementsByClassName("movelist_highlight_blue");
+		if (elements && elements.length > 0) {
+			return elements[0];
+		}
+		elements = document.getElementsByClassName("movelist_highlight_yellow");
+		if (elements && elements.length > 0) {
+			return elements[0];
+		}
+		return null;
+	},
+
+	fix_scrollbar_position: function() {
+		let highlight = this.get_movelist_highlight();
+		if (highlight) {
+			let top = highlight.offsetTop - movelist.offsetTop;
+			if (top < movelist.scrollTop) {
+				movelist.scrollTop = top;
+			}
+			let bottom = top + highlight.offsetHeight;
+			if (bottom > movelist.scrollTop + movelist.offsetHeight) {
+				movelist.scrollTop = bottom - movelist.offsetHeight;
+			}
+		} else {
+			movelist.scrollTop = 0;
+		}
+	},
+};
